19d90943b02758ee68be7b6341cf60564ffb478a
skstlenovo <skstlenovo@gmail.com>
2025-12-02 23:16:11 +0530

    site map included

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,8 @@ export default defineConfig({
   [
     react(),
     tailwindcss(),
+    sitemap({
+      hostname: 'https://www.javabackend.in'
+    })
   ],
 })
